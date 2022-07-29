7703306bc1066dec2b99ab8254e9036f22e49f74
jeoffreyduke <jeoffreyduke@gmail.com>
2022-07-29 21:52:02 +0100

    fixed window bug!

--- next.config.js
+++ next.config.js
@@ -8,3 +8,13 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+module.exports = {
+  webpack: (config) => {
+    // this will override the experiments
+    config.experiments = { ...config.experiments, ...{ topLevelAwait: true } };
+    // this will just update topLevelAwait property of config.experiments
+    // config.experiments.topLevelAwait = true
+    return config;
+  },
+};
